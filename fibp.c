57e0a10548ca20d920372cc2d66b6cc4308c5e18
Ondřej Kováč <96345636+ondrejkovac021@users.noreply.github.com>
2022-06-05 11:07:12 +0200

    Create fibp.c

---
+++ fibp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+int a=0, b=1, num, c;
+
+    for(int i=0; i<num; i++)
+    {
+        printf("%d ",a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+     return 0;
+}
