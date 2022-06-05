6751ade3846acc267770d5a4cdb399368a5dbcfd
Ondřej Kováč <96345636+ondrejkovac021@users.noreply.github.com>
2022-06-05 11:16:33 +0200

    Create vetsimensi.c

---
+++ vetsimensi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 8,b = 31;
+    if(a<b)
+    {
+        printf("%d je větší",b);
+        
+    }
+    else 
+    {
+        printf ("%d je větší",a);
+    }
+}
